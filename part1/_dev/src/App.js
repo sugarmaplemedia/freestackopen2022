c3bb0235d8c3361e67d2fbfac9b10f932fcb04c2
Harrison Bouche <harrison@sm.media>
2023-01-21 21:43:14 -0500

    Built 1.12 anecodes, saved old app.js in 1.11

--- part1/_dev/src/App.js
+++ part1/_dev/src/App.js
@@ -1,62 +1,30 @@
 import { useState } from 'react'
 
-const Button = ({state, setter, children}) => {
-    const updateFeedback = () => {
-        setter(state += 1);
-    }
-
-    return (
-        <button onClick={updateFeedback}>{children}</button>  
-    )
-}
-
-const StatisticLine = ({title, count}) => (
-    <tr>
-        <td>{title}</td>
-        <td>{count}</td>
-    </tr>
-)
-
-const Statistics = ({feedbackCount, counterArray}) => {
-    if (feedbackCount > 0) {
-        return (
-            <table>
-                <tbody>
-                    {counterArray.map(counter => (
-                        <StatisticLine key={counter[0]} title={counter[0]} count={counter[1]} />
-                    ))}
-                </tbody>
-            </table>
-        )
-    } else {
-        return <p>no feedback given</p>
-    }
-}
-
 const App = () => {
-    // save clicks of each button to its own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-    let all = good + neutral + bad
+    const [selected, setSelected] = useState(0)
+    const anecdotes = [
+        'If it hurts, do it more often.',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+        'The only way to go fast, is to go well.'
+    ]
+
+    const getRandomAnecdote = () => {
+        let randomAnecdote;
+        do {
+            randomAnecdote = Math.floor(Math.random() * anecdotes.length)
+        } while (randomAnecdote === selected)
+        setSelected(randomAnecdote)
+    }
 
     return (
         <div>
-            <h1>Give feedback</h1>
-            <Button setter={setGood} state={good}>good</Button>
-            <Button setter={setNeutral} state={neutral}>neutral</Button>
-            <Button setter={setBad} state={bad}>bad</Button>
-            <h2>Statistics</h2>
-            <Statistics feedbackCount={all} counterArray={
-                [
-                    ["good", good],
-                    ["neutral", neutral],
-                    ["bad", bad],
-                    ["all", all],
-                    ["average", `${all === 0 ? "no feedback" : (good - bad) / all}`],
-                    ["positive", `${all === 0 ? "no feedback" : (good / all * 100) + "%"}`]
-                ]
-            } />
+            <p>{anecdotes[selected]}</p>
+            <button onClick={getRandomAnecdote}>next anecdote</button>
         </div>
     )
 }
