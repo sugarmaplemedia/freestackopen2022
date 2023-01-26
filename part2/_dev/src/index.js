07528c545f7881673f750661c9bbbf742e6abcf6
sugarmaplemedia <harrison@sm.media>
2023-01-26 18:14:14 -0500

    Finished Part 2 Forms

--- part2/_dev/src/index.js
+++ part2/_dev/src/index.js
@@ -1,5 +1,31 @@
-import { createRoot } from 'react-dom/client';
-import App from './notes/App';
+// import { createRoot } from 'react-dom/client';
+// import App from './notes/App';
 
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+// const root = createRoot(document.getElementById('root'))
+// root.render(<App />)
+
+import ReactDOM from "react-dom/client"
+
+import App from "./notes/App"
+
+const notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true,
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false,
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true,
+    },
+]
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <App notes={notes} />
+)
