21bf78bee83c7727ba0bb712b3b48ecefb3e9037
Harrison Bouche <harrison@sm.media>
2023-01-29 13:08:03 -0500

    Currently working on 2.12

--- part2/_dev/src/notes/App.js
+++ part2/_dev/src/notes/App.js
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Note from './components/Note'
+import noteService from './services/notes'
 
-const App = (props) => {
-    const [notes, setNotes] = useState(props.notes)
+const App = () => {
+    const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState("a new note...")
     const [showAll, setShowAll] = useState(true)
+
+    useEffect(() => {
+        noteService
+            .getAll()
+            .then(fetchedNotes => {
+                setNotes(fetchedNotes)
+            })
+    }, [])
 
     const addNote = (event) => {
         event.preventDefault();
@@ -13,13 +22,31 @@ const App = (props) => {
             important: Math.random() < 0.5,
             id: notes.length + 1,
         }
-    
-        setNotes(notes.concat(noteObject))
-        setNewNote('')
+
+        noteService
+            .create(noteObject)
+            .then(() => {
+                setNotes(notes.concat(noteObject))
+                setNewNote('')
+            })
+    }
+
+    const toggleImportanceOf = (id) => {
+        const note = notes.find(n => n.id === id)
+        const changedNote = { ...note, important: !note.important }
+
+        noteService
+            .update(id, changedNote)
+            .then(fetchedNote => {
+                setNotes(notes.map(n => n.id !== id ? n : fetchedNote))
+            })
+            .catch(error => {
+                alert(`the note '${note.content}' was already deleted from server`)
+                setNotes(notes.filter(n => n.id !== id))
+            })
     }
 
     const handleNoteChange = (event) => {
-        console.log(event.target.value)
         setNewNote(event.target.value)
     }
 
@@ -35,7 +62,11 @@ const App = (props) => {
             </div>
             <ul>
                 {notesToShow.map(note => 
-                <Note key={note.id} note={note} />
+                <Note 
+                    key={note.id}
+                    note={note}
+                    toggleImportance={() => toggleImportanceOf(note.id)}
+                />
                 )}
             </ul>
             <form onSubmit={addNote}>
--- part2/_dev/src/phonebook/App.js
+++ part2/_dev/src/phonebook/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const Filter = ({ handleFunction }) => (
     <div>
@@ -32,15 +33,18 @@ const List = ({list}) => (
 )
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 0 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
-    ])
+    const [persons, setPersons] = useState([])
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/persons")
+            .then(response =>
+                setPersons(response.data)    
+            )
+    },[])
     const [personsToShow, setPersonsToShow] = useState([])
     useEffect(() => {
         filterText()
+    // eslint-disable-next-line
     }, [persons])
 
     const emptyPerson = {
