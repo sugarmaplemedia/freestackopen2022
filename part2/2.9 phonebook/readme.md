2faa0a958c2c7eb245986291a2396cb1d1c9b87d
Harrison Bouche <harrison@sm.media>
2023-01-31 18:36:38 -0500

    Relocated all project files to vitedev folder

--- part2/2.9 phonebook/readme.md
+++
@@ -1,5 +0,0 @@
-# How to run in command line
-```
-npm ci
-node server.js
-```
--- part2/_dev/src/index.js
+++
@@ -1,5 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './countries/App';
-
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
