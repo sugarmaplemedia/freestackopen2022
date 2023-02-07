c81b13fc1c0fbd7a2f9d7228b19b94d4d72d6d4a
Harrison Bouche <harrison@sm.media>
2023-02-07 08:39:50 -0500

    Merged frontend with backend

--- part3/index.js
+++ part3/index.js
@@ -18,6 +18,7 @@ const unknownEndpoint = (request, response) => {
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
+app.use(express.static("frontend"))
 // app.use(unknownEndpoint)
 
 let notes = [
@@ -37,11 +38,6 @@ let notes = [
         important: true
     }
 ]
-
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
